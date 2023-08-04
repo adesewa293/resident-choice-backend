cc6abfb47aaa4b42921b3668dc6b438fc0abd52a
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-04 15:49:51 +0100

    setup express

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+const port = process.env.PORT || 4000;
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the CareHome Web App!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
